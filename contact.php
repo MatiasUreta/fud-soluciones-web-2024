0950f8b3a2d5cb9680265d1c55d22a26c6c08329
matias <matiasureta28@gmail.com>
2024-05-24 10:09:01 -0300

    agregue variables de entorno

--- contact.php
+++ contact.php
@@ -1,36 +1,46 @@
-
 <?php
 require 'vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-require 'vendor/autoload.php';
+
+// Cargar las variables de entorno
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__, 'variables.env');
+$dotenv->load();
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-$nombre = strtolower(trim($_POST['firstName']));
-$apellido = strtolower(trim($_POST['lastName']));
-$email = strtolower(trim($_POST['email']));
-$telefono = strtolower(trim($_POST['phone']));
-$mensaje = strtolower(trim($_POST['message']));
-$mail = new PHPMailer(true);
-try {
-// Server settings
-$mail->isSMTP();
-$mail->Host = 'smtp.hostinger.com'; // Specify your SMTP server
-$mail->SMTPAuth = true;
-$mail->Username = 'matiasureta28@gmail.com'; // SMTP username
-$mail->Password = 'Ragnarok2409.'; // SMTP password
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-$mail->Port = 465;
-// Recipients
-$mail->setFrom($email, "$nombre $apellido");
-$mail->addAddress('info@fudsolucionesweb.com'); // Add a recipient
-// Content
-$mail->isHTML(false); // Set email format to plain text
-$mail->Subject = 'Nuevo mensaje de tu sitio web';
-$mail->Body = "Nombre: $nombre\nApellido: $apellido\nEmail: $email\nTeléfono: $telefono\nMensaje:\n$mensaje";
-$mail->send();
-echo "Mensaje enviado con éxito";
-} catch (Exception $e) {
-echo "Hubo un error al enviar el mensaje. Mailer Error: {$mail->ErrorInfo}";
-}
+    $nombre = filter_input(INPUT_POST, 'firstName', FILTER_SANITIZE_STRING);
+    $apellido = filter_input(INPUT_POST, 'lastName', FILTER_SANITIZE_STRING);
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    $telefono = filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_STRING);
+    $mensaje = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING);
+
+    $mail = new PHPMailer(true);
+
+    // Habilitar la depuración
+    $mail->SMTPDebug = 2;
+
+    try {
+        // Configuración del servidor
+        $mail->isSMTP();
+        $mail->Host = getenv('SMTP_HOST');
+        $mail->SMTPAuth = true;
+        $mail->Username = getenv('SMTP_USERNAME');
+        $mail->Password = getenv('SMTP_PASSWORD');
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port = 465;
+
+        // Destinatarios
+        $mail->setFrom($email, "$nombre $apellido");
+        $mail->addAddress('info@fudsolucionesweb.com');
+
+        // Contenido
+        $mail->isHTML(false);
+        $mail->Subject = 'Nuevo mensaje de tu sitio web';
+        $mail->Body = "Nombre: $nombre\nApellido: $apellido\nEmail: $email\nTeléfono: $telefono\nMensaje:\n$mensaje";
+        $mail->send();
+        echo "Mensaje enviado con éxito";
+    } catch (Exception $e) {
+        error_log("Hubo un error al enviar el mensaje. Mailer Error: {$mail->ErrorInfo}");
+    }
 }
 ?>
